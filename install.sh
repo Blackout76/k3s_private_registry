a3e9b4cab82520457ae4d817fbd4c9e308e121a5
Blackout <olivier.leger.dev@gmail.com>
2024-03-30 15:56:24 +0100

    update

--- install.sh
+++ install.sh
@@ -1,29 +1,28 @@
 #!/bin/bash
 
 echo "Enter domain name:"
-DOMAIN=""
-# read DOMAIN
+# DOMAIN=""
+read DOMAIN
 
 echo "Enter valid email (domain certs):"
-CERT_MAIL=""
-# read CERT_MAIL
+# CERT_MAIL=""
+read CERT_MAIL
 
 echo "Enter docker username:"
-DOCKER_LOGIN="docker"
-# read LOGIN
+# DOCKER_LOGIN="docker"
+read LOGIN
 
 echo "Enter docker password:"
-DOCKER_PASSWORD="docker"
-# read PASSWORD
-
+# DOCKER_PASSWORD="docker"
+read PASSWORD
 
 echo "Enter docker registry port:"
-DOCKER_PORT=30500
-# read DOCKER_PORT
+# DOCKER_PORT=30500
+read DOCKER_PORT
 
 echo "Enter docker registry volume path:"
-DOCKER_PVC="/volumes/docker_registry"
-# read DOCKER_PVC
+#DOCKER_PVC="/volumes/docker_registry"
+read DOCKER_PVC
 
 
 apt-get install apache2-utils -Y
@@ -46,15 +45,21 @@ echo "Generating auth secret"
 kubectl create secret generic docker-registry-auth-secret --from-file=registry/auth/htpasswd --namespace docker-registry
 
 echo "Deploy storage"
-file_contents=$(</deploys/docker-storage.yaml)
-echo "${file_contents//__DOCKER_PVC__/$DOCKER_PVC}" > /deploys/docker-storage.yaml
+file_contents=$(<deploys/docker-storage.yaml)
+echo "${file_contents//__DOCKER_PVC__/$DOCKER_PVC}" > deploys/docker-storage.yaml
 kubectl apply -f deploys/docker-storage.yaml
 
-# echo "Deploy docker registry"
+echo "Deploy docker registry"
+file_contents=$(<deploys/docker-storage.yaml)
+echo "${file_contents//__DOCKER_PORT__/$DOCKER_PORT}" > deploys/docker-registry.yaml
 # kubectl apply -f deploys/docker-registry.yaml
 
-# echo "Deploy docker domain tls"
-# kubectl apply -f deploys/docker-domain-cert.yaml
+echo "Deploy docker domain tls"
+file_contents=$(<deploys/docker-storage.yaml)
+file_contents=${file_contents//__DOMAIN__/$DOMAIN}
+file_contents=${file_contents//__MAIL__/$CERT_MAIL}
+echo "$file_contents" > deploys/docker-domain-cert.yaml
+kubectl apply -f deploys/docker-domain-cert.yaml
 
-# echo "Deploy docker ingress"
-# kubectl apply -f deploys/docker-ingress.yaml
+echo "Deploy docker ingress"
+kubectl apply -f deploys/docker-ingress.yaml
