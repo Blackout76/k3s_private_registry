81ac1e72513ee1d7be6d2424f5a0451a752a88a9
Blackout <olivier.leger.dev@gmail.com>
2024-03-30 13:59:07 +0100

    update

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # k3s_private_registry
+
+## Install
+
+```
+git clone https://github.com/Blackout76/k3s_private_registry.git
+
+cd k3s_private_registry
+
+chmod +x install.sh
+
+sudo ./install.sh
+```
+
+## access
+
+Catalog: `https://<your domain>/v2/_catalog/`
--- install.sh
+++ install.sh
@@ -7,15 +7,16 @@ echo "Enter docker user:"
 LOGIN="docker"
 # read LOGIN
 echo "Enter password user:"
-PASSWORD="docker1234656"
+PASSWORD="docker"
 # read PASSWORD
 
 
 apt-get install apache2-utils -Y
 
-mkdir -p ./registry/certs ./registry/auth ./deploys
+mkdir -p ./registry/certs ./registry/auth
 
 openssl req -x509 -newkey rsa:4096 -days 3650 -nodes -sha256 -keyout registry/certs/tls.key -out registry/certs/tls.crt -subj "/CN=docker-registry"
+htpasswd -Bbn "$LOGIN" "$PASSWORD" > registry/auth/htpasswd
 
 echo ""
 echo "Kubernetes deployment ..."
@@ -26,8 +27,17 @@ kubectl create namespace docker-registry
 echo "Generating certs secret"
 kubectl create secret tls docker-registry-certs-secret --cert=registry/certs/tls.crt --key=registry/certs/tls.key --namespace docker-registry
 
-echo "Generating certs secret"
+echo "Generating auth secret"
 kubectl create secret generic docker-registry-auth-secret --from-file=registry/auth/htpasswd --namespace docker-registry
 
 echo "Deploy storage"
 kubectl apply -f deploys/docker-storage.yaml
+
+echo "Deploy docker registry"
+kubectl apply -f deploys/docker-registry.yaml
+
+echo "Deploy docker domain tls"
+kubectl apply -f deploys/docker-domain-cert.yaml
+
+echo "Deploy docker ingress"
+kubectl apply -f deploys/docker-ingress.yaml
